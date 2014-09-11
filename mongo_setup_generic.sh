acb8b6c86b00e58ec47a7f10e7f4304e449d6555
Charlie Page <charlie.page@10gen.com>
2014-09-11 15:02:02 -0400

    mongo_setup_generic.sh

---
+++ mongo_setup_generic.sh
@@ -0,0 +1,54 @@
+sudo su -
+
+#AMAZON instances remove the cloud config
+umount /dev/xvdb
+sed -i '/cloudconfig/d' /etc/fstab
+
+sudo cat << EOF > /etc/rc.local
+#!/bin/sh -e
+
+if test -f /sys/kernel/mm/transparent_hugepage/enabled; then
+   echo never > /sys/kernel/mm/transparent_hugepage/enabled
+   echo madvise > /sys/kernel/mm/transparent_hugepage/enabled
+fi
+if test -f /sys/kernel/mm/transparent_hugepage/defrag; then
+   echo never > /sys/kernel/mm/transparent_hugepage/defrag
+   echo madvise > /sys/kernel/mm/transparent_hugepage/defrag
+fi
+
+echo 0 > /proc/sys/vm/zone_reclaim_mode
+
+exit 0
+EOF
+
+#disable NUMA
+#assumes Amazon linux
+sed -i '/^kernel/ s/$/ numa=off/g' /etc/grub.conf
+#assumes ubuntu
+sed -i '/^kernel/ s/$/ numa=off/g' /boot/grub/menu.lst
+sed -i '/^kernel/ s/$/ apparmor=0/g' /boot/grub/menu.lst
+
+#disable SE linux
+#this needs to be updated to change anything to disabled, permissive sucks to (still does work)
+if [ -f /etc/selinux/config ]; then
+sudo sed -i 's/enforcing/disabled/' /etc/selinux/config
+fi
+
+#set keepalive
+echo "net.ipv4.tcp_keepalive_time = 300" >> /etc/sysctl.conf
+sysctl -p
+
+#set ulimits
+echo "* soft nofile 64000
+* hard nofile 64000
+* soft nproc 32000
+* hard nproc 32000" > /etc/security/limits.conf
+#various RHEL clones set things in limits.d, we don't want any of them
+rm -rf /etc/security/limits.d/*
+
+#set RA
+#NOTE: On Amazon read_ahead_kb is getting translated to 2x for RA (becuause SSZ = 512?)
+cat <<EOF>> /etc/udev/rules.d/51-ec2-hvm-devices.rules
+SUBSYSTEM=="block", ACTION=="add|change", ATTR{bdi/read_ahead_kb}="16", ATTR{queue/scheduler}="noop"
+EOF
+
