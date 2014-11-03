f1614e38c37a9115d4a2d7b310ea4a1a754fb76f
Charlie Page <charlie.page@10gen.com>
2014-11-03 15:52:13 -0500

    update

--- build_mongod.sh
+++ build_mongod.sh
@@ -89,6 +89,15 @@ service mongod stop
 update-rc.d mongod disable
 }
 
+mongoinstallubuntu26() {
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
+echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+service mongod stop
+update-rc.d mongod disable
+}
+
 disksetup() {
 for mydir in $(seq $DISKSTART $DISKEND); do
   mkdir -p /data/$mydir
