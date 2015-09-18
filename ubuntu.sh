7094deb57420987d245500f31e013676292b06ad
Charlie Page <charlie.page@gmail.com>
2015-09-18 15:00:57 -0400

    Create ubuntu.sh

---
+++ ubuntu.sh
@@ -0,0 +1,6 @@
+sudo apt-get install linux-firmware-nonfree compizconfig-settings-manager
+#sudo apt-get install nvidia-current
+#sudo apt-get install fglrx fglrx-updates 
+#sudo invoke-rc.d apparmor kill
+#sudo update-rc.d -f apparmor remove
+
