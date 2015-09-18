4991b3f32bc632c68b1e0b716ce9dfb35a647c27
Charlie Page <charlie.page@gmail.com>
2015-09-18 15:14:13 -0400

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,6 +1,23 @@
-sudo apt-get install linux-firmware-nonfree compizconfig-settings-manager
+#!/bin/bash
+#Ubuntu
+sudo apt-get install linux-firmware-nonfree compizconfig-settings-manager unity-tweak-tool
+
+#Video cards
 #sudo apt-get install nvidia-current
 #sudo apt-get install fglrx fglrx-updates 
+
+#App armor
 #sudo invoke-rc.d apparmor kill
 #sudo update-rc.d -f apparmor remove
+
+#System utils
+sudo apt-get install htop zip unzip 
+
+#Java
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
+
+
+sudo apt-get -f install && sudo apt-get autoremove && sudo apt-get -y autoclean && sudo apt-get -y clean
 
